2fb11a2f6f9e0b2532e042e74a64b6fd872958d7
Sandeep Kadam <105204457+Sandeep-Kadam8@users.noreply.github.com>
2022-05-18 09:58:29 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,26 @@
 #include "stats.h"
+#include <algorithm>
+#include <cmath>
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
-    //Implement statistics here
+Stats::Stats()
+    :average{0}, max{0},min{0}
+{
+
+}
+
+Stats Statistics::ComputeStatistics(const std::vector<float>& list) {
+    Stats stat;
+    if(!list.empty()) {
+        stat.min = *std::min_element(list.begin(), list.end());
+        stat.max = *std::max_element(list.begin(), list.end());
+        for(int x = 0; x < list.size(); ++x) {
+            stat.average += list[x];
+        }
+        stat.average /= list.size();
+    } else {
+        stat.average = std::nan(NULL);
+        stat.max = std::nan(NULL);
+        stat.min = std::nan(NULL);
+    }
+    return stat;
 }
